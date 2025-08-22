8304e328c02a63e80ecc07f5ad0f23b82f23f577
oatsfx <30326975+oatsfx@users.noreply.github.com>
2025-08-21 21:49:28 -0700

    fix: naming

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -4,7 +4,7 @@ public static class ModHelperData
 {
     public const string WorksOnVersion = "49.3";
     public const string Version = "1.0.0";
-    public const string Name = "AbilitySeconds";
+    public const string Name = "Ability Seconds";
 
     public const string Description = "Display tower ability cooldown timers on top of their buttons.";
 
